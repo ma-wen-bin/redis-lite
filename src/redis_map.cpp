505fbbf5d2361f60a2ec19b81dee7370f3d0b93b
ma-wen-bin <wenbinma2006@gmail.com>
2026-09-07 01:48:33 +0800

    implementation for redis object (basic string operations)

--- src/redis_map.cpp
+++ src/redis_map.cpp
@@ -9,65 +9,142 @@ std::vector<Response> RedisMap::processRequestQueue(std::queue<Request>& request
 }
 
 void RedisMap::performRequest(const Request &request)
-{
-    switch (request.getType())
-    {
-    case RequestType::SET:
-        setKeyValue(request);
-        break;
-    case RequestType::GET:
-        getValue(request);
-        break;
-    case RequestType::DEL:
-        deleteValue(request);
-        break;
-    case RequestType::NONE:
-        std::cout << "Request type does not exist. Dropping request!";
+{   
+    switch (request.getType()) {
+        
+        // BASIC OPERATIONS
+        case RequestType::SET: 
+            set(request);
+            break;
+        case RequestType::GET: 
+            get(request);
+            break;
+        case RequestType::DEL: 
+            del(request);
+            break;
+        case RequestType::STRLEN: 
+            strlen(request);
+            break;    
+        case RequestType::APPEND: 
+            append(request);
+            break;        
+        
+        // LIST OPERATIONS
+        case RequestType::LPUSH: 
+            lpush(request);
+            break;
+        case RequestType::RPUSH: 
+            rpush(request);
+            break;
+        case RequestType::LRANGE: 
+            lrange(request);
+            break;
+        
+        // HASH OPERATIONS
+        case RequestType::HDEL: 
+            hdel(request);
+            break;
+        case RequestType::HSET: 
+            hset(request);
+            break;
+        case RequestType::HGET: 
+            hget(request);
+            break;
+
+        // SET OPERATIONS
+        case RequestType::SADD: 
+            sadd(request);
+            break;
+        case RequestType::SMEMBERS: 
+            smembers(request);
+            break;
+        case RequestType::SCARD: 
+            scard(request);
+            break;            
+        case RequestType::SISMEMBER: 
+            sismember(request);
+            break;   
+
+        case RequestType::NONE:
+            std::cout << "Request type does not exist. Dropping request!";    
     }
 }
 
-// SET
-void RedisMap::setKeyValue(const Request &request)
-{
-    if (!request.getKey().empty() && !request.getValue().empty())
-    {
-        redisMap[request.getKey()] = request.getValue();
-        std::cout << "Key: " << request.getKey() << " with value: " << request.getValue() << " was inserted." << '\n';
+
+////////// BASIC OPERATIONS (START)
+void RedisMap::get(const Request &request) {
+    if (!request.getKey().empty()) {
+        auto iterator = redisMap.find(request.getKey());
+        if (iterator != redisMap.end()) {
+            std::shared_ptr<RedisObjectString> stringObjectPtr = std::dynamic_pointer_cast<RedisObjectString>(redisMap[request.getKey()]);
+            if (stringObjectPtr != nullptr) {
+                std::string value = stringObjectPtr->getValue();
+                std::cout << "Key: " << request.getKey() << " with value: " << value << " was retrieved." << '\n';
+                processedResponses.push_back(Response(ResponseRespType::BulkString, value));
+            }
+            std::cout << "Redis object retireved does not exist!" << '\n';
+        }
+    }
+}
+
+void RedisMap::set(const Request &request) {
+    if (!request.getKey().empty()) {
+        auto iterator = redisMap.find(request.getKey());
+        if (iterator != redisMap.end()) {
+            std::shared_ptr<RedisObjectString> stringObjectPtr = std::dynamic_pointer_cast<RedisObjectString>(redisMap[request.getKey()]);
+            if (stringObjectPtr != nullptr) {
+                stringObjectPtr->setValue(request.getArguments()[1]);
+                std::cout << "Key: " << request.getKey() << " updated with value: " << request.getArguments()[1] << '\n';
+            }
+        }
+
+        redisMap[request.getKey()] = std::make_shared<RedisObjectString>(request.getArguments()[1]);
+        std::cout << "Key: " << request.getKey() << " with value: " << request.getArguments()[1] << " inserted." << '\n';
         processedResponses.push_back(Response(ResponseRespType::SimpleString, "OK"));
     }
 }
 
-// GET
-void RedisMap::getValue(const Request &request)
-{
-    if (!request.getKey().empty())
-    {   
+void RedisMap::del(const Request &request) {
+    if (!request.getKey().empty()) {
         auto iterator = redisMap.find(request.getKey());
         if (iterator != redisMap.end()) {
-            std::string value = redisMap[request.getKey()];
-            std::cout << "Key: " << request.getKey() << " with value: " << value << " was retrieved." << '\n';
-            processedResponses.push_back(Response(ResponseRespType::BulkString, value));
+            redisMap.erase(request.getKey());
+            std::cout << "Key: " << request.getKey() << " was successfully deleted." << '\n';
+            processedResponses.push_back(Response(ResponseRespType::Integer, 1)); 
             return;
         }
         std::cout << "Key: " << request.getKey() << " does not exist." << '\n';
+        processedResponses.push_back(Response(ResponseRespType::Integer, 0)); 
     }
 }
 
-// DEL
-void RedisMap::deleteValue(const Request &request)
-{
-    size_t removed;
-    if (!request.getKey().empty())
-    {
-        removed = redisMap.erase(request.getKey());
+void RedisMap::strlen(const Request &request) {
+    if (!request.getKey().empty()) {
+        auto iterator = redisMap.find(request.getKey());
+        if (iterator != redisMap.end()) {
+            std::shared_ptr<RedisObjectString> stringObjectPtr = std::dynamic_pointer_cast<RedisObjectString>(redisMap[request.getKey()]);
+            processedResponses.push_back(Response(ResponseRespType::Integer, stringObjectPtr->strlen())); 
+            return;
+        }
+        std::cout << "Key: " << request.getKey() << " does not exist." << '\n';
+        processedResponses.push_back(Response(ResponseRespType::Integer, 0)); 
     }
-
-    if (removed)
-    {
-        std::cout << "Key: " << request.getKey() << " was successfully deleted." << '\n';
-        processedResponses.push_back(Response(ResponseRespType::Integer, 1)); //DELTETION TOOK PLACE
-        return;
-    }
-    std::cout << "Key: " << request.getKey() << " does not exist." << '\n';
-    processedResponses.push_back(Response(ResponseRespType::Integer, 0)); //NO DELETEION TOOK PLACE
 }
+
+void RedisMap::append(const Request &request) {
+    if (!request.getKey().empty()) {
+        auto iterator = redisMap.find(request.getKey());
+        if (iterator != redisMap.end()) {
+            std::shared_ptr<RedisObjectString> stringObjectPtr = std::dynamic_pointer_cast<RedisObjectString>(redisMap[request.getKey()]);
+            stringObjectPtr->append(request.getArguments()[2]);
+            processedResponses.push_back(Response(ResponseRespType::Integer, stringObjectPtr->strlen())); 
+            return;
+        }
+        std::cout << "Key: " << request.getKey() << " does not exist." << '\n';
+        processedResponses.push_back(Response(ResponseRespType::Integer, 0)); 
+    }
+}
+
+////////// BASIC OPERATIONS (END)
+
+
--- src/redis_map.h
+++ src/redis_map.h
@@ -3,19 +3,46 @@
 #include <string>
 #include <queue>
 #include <charconv>
+#include <memory>
+
 #include "request.h"
 #include "response.h"
+#include "redis_obj.h"
+
 
 class RedisMap {
     private:
-    inline static std::unordered_map<std::string, std::string> redisMap;
+    inline static std::unordered_map<std::string, std::shared_ptr<RedisObject>> redisMap;
     std::vector<Response> processedResponses; 
 
     public:
     std::vector<Response> processRequestQueue(std::queue<Request>& requestQueue);
+
+    // BASE HANDLER
     void performRequest(const Request &request);
-    void setKeyValue(const Request &request);
-    void getValue(const Request &request);
-    void deleteValue(const Request &request);
+
+    // BASIC OPERATIONS 
+    void get(const Request &request);
+    void set(const Request &request);
+    void del(const Request &request);
+    void strlen(const Request &request);
+    void append(const Request &request);
+
+
+    // LIST OPERATIONS
+    void lpush(const Request &request);
+    void rpush(const Request &request);
+    void lrange(const Request &request);
+
+    // HASH OPERATIONS
+    void hdel(const Request &request);
+    void hset(const Request &request);
+    void hget(const Request &request);
+    
+    // SET OPERATIONS
+    void sadd(const Request &request);
+    void smembers(const Request &request);
+    void scard(const Request &request);
+    void sismember(const Request &request);
 
 };
